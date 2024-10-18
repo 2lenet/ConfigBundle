f8373baadf91b842693d4b5ef0612303862bde6f
valentin-helies <89925635+valentin-helies@users.noreply.github.com>
2024-10-18 16:36:44 +0200

    Update LleConfigBundle.php

--- src/LleConfigBundle.php
+++ src/LleConfigBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LleConfigBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
     }
